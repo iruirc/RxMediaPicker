b00efb0a896ffe59933d3ce85ad5ae4a6e33a33d
Nuzhin Dmitry <iruirc@gmail.com>
2022-04-04 16:26:08 +0300

    Update url for package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RxMediaPicker",
+    url: "https://github.com/iruirc/RxMediaPicker.git"
     platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
